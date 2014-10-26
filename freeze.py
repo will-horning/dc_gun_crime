b9cdf0706b8b5afe5921ef1509a8416ed856c98c
Will <will.horning@gmail.com>
2014-10-25 22:38:16 -0400

    Modified freeze.py script to copy index.html and bundled javascript and css out of build directory after calling freezer.Freeze()

--- freeze.py
+++ freeze.py
@@ -1,3 +1,4 @@
+import shutil
 from app import app
 from flask_frozen import Freezer
 
@@ -5,3 +6,6 @@ freezer = Freezer(app)
 
 if __name__ == '__main__':
 	freezer.freeze()
+	shutil.copy2('./build/index.html', './index.html')
+	shutil.copy2('./build/static/bundle.js', './static/bundle.js')
+	shutil.copy2('./build/static/bundle.css', './static/bundle.css')
