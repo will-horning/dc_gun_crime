e71aa299b14f0326483324f22ae85420c83e7f65
Will <will.horning@gmail.com>
2014-10-26 13:36:29 -0400

    Set Frozen-flask directory to root, no longer copying files over from build.

--- freeze.py
+++ freeze.py
@@ -1,11 +1,9 @@
-import shutil
 from app import app
 from flask_frozen import Freezer
 
+app.config['FREEZER_DESTINATION'] = './'
+app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-	freezer.freeze()
-	shutil.copy2('./build/index.html', './index.html')
-	shutil.copy2('./build/static/bundle.js', './static/js/lib/bundle.js')
-	shutil.copy2('./build/static/bundle.css', './static/css/bundle.css')
+    freezer.freeze()
