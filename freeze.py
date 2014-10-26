ed30445c5bb231d01eddf8091a47a0cf42f87a39
Will <will.horning@gmail.com>
2014-10-26 13:54:02 -0400

    Figured out to do bundling without frozen-flask: moved Flask template path to root, set Flask-assets to not use querystring for versioning, jinja no longer needed

--- freeze.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flask_frozen import Freezer
-
-app.config['FREEZER_DESTINATION'] = './'
-app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
